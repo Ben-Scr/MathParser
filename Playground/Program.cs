c6d89dce6aaf7ca436cba5baab8f5966787888ce
rrainix <benscr.email@gmail.com>
2025-11-01 17:03:56 +0100

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -7,16 +7,12 @@ public static class Program
         Console.WriteLine("-- Calculator --");
         Console.WriteLine("Enter your calculation:");
 
-        int resultLength = 30;
 
         while (true)
         {
-            for (int i = 0; i < resultLength; i++)
-                Console.Write("-");
-            Console.WriteLine();
+            Console.Write("------------------------------------");
             string input = Console.ReadLine();
             string result = input + " = " + Calculator.Evaluate(input);
-            resultLength = result.Length;
             Console.WriteLine(result);
             Console.WriteLine();
         }
